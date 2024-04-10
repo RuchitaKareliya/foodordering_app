f2f175f68f9257d80cfbc077da981f28375fcd00
RuchitaKareliya <rkareliya965@rku.ac.in>
2024-04-10 22:26:09 +0530

    App_UIDesign

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodordering_app/Views/splash_Screen/splash_screen.dart';
 import 'package:get/get.dart';
 import 'consts/consts.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
